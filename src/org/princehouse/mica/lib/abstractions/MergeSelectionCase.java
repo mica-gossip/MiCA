0317799c2bef1dad45f627b5508c51b24a054629
Lonnie Princehouse <lonnie.princehouse@gmail.com>
2012-04-29 12:26:47 -0400

    merge now supports preUpdate, postUpdate

---
+++ src/org/princehouse/mica/lib/abstractions/MergeSelectionCase.java
@@ -0,0 +1,8 @@
+package org.princehouse.mica.lib.abstractions;
+
+public enum MergeSelectionCase {
+	NA, //no selection has yet been made this round
+	P1, // gossip only p1
+	P2, // gossip only p2
+	BOTH  // gossip both
+}
