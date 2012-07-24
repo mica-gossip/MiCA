2046773a114c5fcf4cd0d63a04ec9c9f42e08179
Lonnie Princehouse <lonnie.princehouse@gmail.com>
2012-07-23 21:38:12 -0400

    -

---
+++ src/org/princehouse/mica/model/NeedsRuntimeException.java
@@ -0,0 +1,19 @@
+package org.princehouse.mica.model;
+
+import org.princehouse.mica.base.simple.SelectException;
+
+/**
+ * Thrown by Selector.asDistribution and Overlay.getOverlay if they are called without a RuntimeState object and it turns out to be necessary
+ *  (not all objects will require RuntimeState to produce a distribution)
+ *   
+ * @author lonnie
+ *
+ */
+public class NeedsRuntimeException extends SelectException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
