209498a702d01e79ddcf9498bba8bfed3575750d
Lonnie Princehouse <lonnie@cs.cornell.edu>
2012-09-24 14:43:47 -0400

    -p

--- src/org/princehouse/mica/example/pulse/StagedTrigger.java
+++ src/org/princehouse/mica/example/pulse/StagedTrigger.java
@@ -1,9 +1,21 @@
 package org.princehouse.mica.example.pulse;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
 import org.princehouse.mica.base.BaseProtocol;
+import org.princehouse.mica.base.annotations.GossipUpdate;
 import org.princehouse.mica.base.annotations.View;
+import org.princehouse.mica.base.net.model.Address;
+import org.princehouse.mica.base.simple.SelectException;
+import org.princehouse.mica.base.simple.Selector;
+import org.princehouse.mica.util.Distribution;
+import org.princehouse.mica.util.Functional;
 
 /**
+ *    incomplete!
+ *    
  * MO: Attempt to communicate round-robin with all nodes in the supplied overlay view
  * 
  * 
@@ -14,6 +26,8 @@ import org.princehouse.mica.base.annotations.View;
 public class StagedTrigger extends BaseProtocol {
 	private static final long serialVersionUID = 1L;
 	
+	private int i = 0; // cursor into view
+	
 	public Object view;
 	
 	public StagedTrigger(Object view) {
@@ -21,7 +35,33 @@ public class StagedTrigger extends BaseProtocol {
 		
 	}
 	
+	private List<Address> getSortedView() {
+		Distribution<Address> dist;
+		try {
+			dist = Selector.asDistribution(view, getRuntimeState());
+		} catch (SelectException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+		Set<Address> keys = dist.keySet();
+		List<Address> lst = Functional.list(keys);
+		Collections.sort(lst);
+		return lst;
+	}
 	
+	
+	@View
+	public Address getNextComm() {
+		List<Address> sview = getSortedView();
+		int index = (i++) % sview.size();
+		return sview.get(index);
+	}
+	
+	@GossipUpdate
+	public void update(StagedTrigger that) {
+		// TODO NOT IMPLEMENTED
+	}
 	
 	
 }
